8808fa67d55e52a13fdbdd5f1b64a1526af0e107
rafals <rszwedowski@advaoptical.com>
2018-07-06 21:19:10 +0200

    added configs for tmux and zsh along with docker extensions

--- setup.sh
+++ setup.sh
@@ -1,9 +1,15 @@
 
 
+# Check if zsh is already installed - or add option for it
+# todo 
+
+# install zsh
+#sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
 
 
 # Symlink all of our dotfiles to the home directory
-for config in vimrc
+for config in vimrc tmux.conf zshrc
 do
 	echo "Creating symlink for $config - ($(pwd)/$config -> $HOME/.$config)"
   ln -sf $(pwd)/$config $HOME/.$config
